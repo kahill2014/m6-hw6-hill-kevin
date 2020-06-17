c13500e07dd080467ddd6d1fe63a79b24894d1a8
Kevin Hill <kahill2014@gmail.com>
2020-06-17 16:30:25 -0500

    Improve code readability with uniform tabs and spacing

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ xmlhttp.onreadystatechange = function() {
 		// Add weather description to main text
 		var weatherNode = document.createTextNode(apiResult.weather[0].description);
 		document.getElementById("weather-details").appendChild(weatherNode);
-
     }
 };
 xmlhttp.open('GET', 'http://api.openweathermap.org/data/2.5/weather?q=53190,us&appid=6efff70fe1477748e31c17d1c504635f', true);
